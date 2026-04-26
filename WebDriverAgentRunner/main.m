e0e2d41fba41445cf60bf515815ce0602308f3b9
Jo tom <wst2019@163.com>
2026-04-26 09:37:53 +0800

    fix: embed FBWDAAppDelegate in UITestingUITests.m and load from xctest bundle in main.m

--- WebDriverAgentRunner/main.m
+++ WebDriverAgentRunner/main.m
@@ -1,7 +1,26 @@
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+static Class loadClassFromXCTestBundle(NSString *className) {
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"WebDriverAgentRunner" ofType:@"xctest" inDirectory:nil];
+    if (bundlePath) {
+        NSBundle *xctestBundle = [NSBundle bundleWithPath:bundlePath];
+        if (xctestBundle && [xctestBundle load]) {
+            Class cls = NSClassFromString(className);
+            if (cls) {
+                return cls;
+            }
+        }
+    }
+    return nil;
+}
 
 int main(int argc, char * argv[]) {
-  @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, @"FBWDAAppDelegate");
-  }
+    @autoreleasepool {
+        Class appDelegateClass = loadClassFromXCTestBundle(@"FBWDAAppDelegate");
+        if (appDelegateClass) {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass(appDelegateClass));
+        }
+        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    }
 }
