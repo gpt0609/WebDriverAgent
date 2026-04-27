f8691f70aab9b85ca3c2797ad3e57daf10385ff0
gpt0609 <136060096+gpt0609@users.noreply.github.com>
2026-04-27 08:14:08 +0800

    Update WebDriverAgentRunner/main.m - WDA App mode with auto-start and keepalive

--- WebDriverAgentRunner/main.m
+++ WebDriverAgentRunner/main.m
@@ -1,7 +1,13 @@
 #import <UIKit/UIKit.h>
 
+@interface WDAApplication : UIApplication
+@end
+
+@implementation WDAApplication
+@end
+
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, @"FBWDAAppDelegate");
+        return UIApplicationMain(argc, argv, NSStringFromClass([WDAApplication class]), @"WDADelegate");
     }
 }
